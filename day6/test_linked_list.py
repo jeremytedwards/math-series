d5323fa50418d2c8a35f89e3c2bc36aa464b8261
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-07 15:39:13 -0800

    Added test for pop wrote pop Added test for insert wrote insert

--- day6/test_linked_list.py
+++ day6/test_linked_list.py
@@ -8,11 +8,24 @@ def test_empty_list():
     assert LinkedList().length == 0
 
 
-#
-#
-# TEST_POPNODE = {
-#     (, )
-# }
-#
-# @pytest.mark.parametrize("fn, result", TEST_NEWNODE)
-# def test_pop(fn, result):
+def test_insert():
+    from linked_list import LinkedList
+    a_list = LinkedList()
+    a_list.insert(1)
+    assert list(a_list) == [1]
+    a_list.insert(2)
+    assert list(a_list) == [2, 1]
+
+
+TEST_POP = {
+    (1, 1),
+    (55, 55),
+    (123490872988582498572379827512423, 123490872988582498572379827512423)
+}
+
+@pytest.mark.parametrize("node_value, result", TEST_POP)
+def test_pop(node_value, result):
+    from linked_list import LinkedList
+    a_list = LinkedList()
+    a_list.insert(node_value)
+    assert a_list.pop() == result
